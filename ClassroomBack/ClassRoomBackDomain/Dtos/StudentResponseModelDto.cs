52ea1f02573c24a4d172ebeda17e8c3218eaf912
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-17 21:38:03 -0300

    Added new field to StudentDto

--- ClassroomBack/ClassRoomBackDomain/Dtos/StudentResponseModelDto.cs
+++ ClassroomBack/ClassRoomBackDomain/Dtos/StudentResponseModelDto.cs
@@ -9,6 +9,7 @@ namespace ClassRoomBackDomain.Dtos
     {
         public string Name { get => throw new NotImplementedException(Name); set => throw new NotImplementedException(); }
         public string Surname { get => throw new NotImplementedException(Surname); set => throw new NotImplementedException(); }
+        public string Class { get => throw new NotImplementedException(Class); set => throw new NotImplementedException(); }
         
     }
 }
