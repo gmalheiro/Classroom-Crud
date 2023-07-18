343fd27f8d8777fa3c1743f78ef148ff6a699def
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-17 21:38:30 -0300

    Fixed StudentResponseModelDto

--- ClassroomBack/ClassRoomBackDomain/Dtos/StudentResponseModelDto.cs
+++ ClassroomBack/ClassRoomBackDomain/Dtos/StudentResponseModelDto.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace ClassRoomBackDomain.Dtos
 {
-    public class StudentResponseModelDto
+    public class StudentResponseModelDTO
     {
         public string Name { get => throw new NotImplementedException(Name); set => throw new NotImplementedException(); }
         public string Surname { get => throw new NotImplementedException(Surname); set => throw new NotImplementedException(); }
