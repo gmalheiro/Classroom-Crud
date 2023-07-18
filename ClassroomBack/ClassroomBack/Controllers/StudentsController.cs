58d217c44a120a60ca1dddf9be82f59f5aa93210
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-17 21:00:31 -0300

    Controller created

---
+++ ClassroomBack/ClassroomBack/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassroomBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public StudentsController(IConfiguration configuration)
+        {
+
+            _configuration = configuration; 
+
+        }
+
+        [HttpGet]
+        [Route("/ListAll")]
+        public string ListAll()
+        {
+            return "Hi";
+        }
+
+    }
+}
