eb8ea129e9820eff51a27559c39e9a765c88215c
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-07-18 23:56:57 -0300

    Added repository class with

--- ClassroomBack/ClassRoomBackDomain/Repository/StudentsRepository.cs
+++ ClassroomBack/ClassRoomBackDomain/Repository/StudentsRepository.cs
@@ -2,11 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassRoomBackDomain.Context;
 
 namespace ClassRoomBackDomain.Repository
 {
     public class StudentsRepository
     {
+        private readonly AppDbContext _context;
+
         
+        public StudentsRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Dtos.StudentResponseModelDTO> ListAll()
+        {
+            return _context.Students.Select( s => new Dtos.StudentResponseModelDTO()
+            {
+                Name = s.Name,
+                Surname = s.Surname,
+                Class = s.Class.ClassName
+            }).ToList();
+        }
+
     }
 }
